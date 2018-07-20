b210293492175cac9ac5cd97f92680dbed96c174
MARCUS VILARONGA <MARCUS VILARONGA@MARCUSVILARONGA.localdomain>
2018-07-19 22:21:09 -0300

    2018-07-19 L13 Individual Assignment

--- CityOfAaron/src/buyi/cit260/curiousworkmanship/view/HelpMenuView.java
+++ CityOfAaron/src/buyi/cit260/curiousworkmanship/view/HelpMenuView.java
@@ -59,17 +59,55 @@ public class HelpMenuView extends MenuView  {
                 howToMoveLocation();
                 break;
             case 6:   // the program will return to the main menu here
-                backHelpMenu();
+                backMainpMenu();
                 break;
             case 7:
                 System.out.println("Quit.");
         }
     }
 
-public void goalOfGame(){System.out.println("\n Shows goals of the game");}
-public void whereIsTheCity(){System.out.println("\n Shows where is the city of Aaron");}
-public void howToviewTheMap(){System.out.println("\n Shows the map");}
-public void howToMoveLocation(){System.out.println("\n Shows how do I move to another location");}
-public void howToDisplayStorehouse(){System.out.println("\n Shows How do I display a list of animals, provisions and tools in the city storehouse");}
-public void backHelpMenu(){System.out.println("\n Back to the Help Menu");}
+public void goalOfGame(){System.out.println(
+        "- In this game the player will assume the role of the ruler over the city Of Aaron." + 
+        "\n- The player of the game will assume the role of the leader over the city of Aaron." + 
+        "\n- Wheat is the staff of life, and is used as the main currency in the city." + 
+        "\n- As ruler over the city, the player’s task is to manage the village’s wheat crops so" +
+        "\nthat the people of the village can be adequately fed, while dealing with rats, and random" +
+        "\ncrop yields." +
+        "\n- The city is blessed when the people pay their tithes and offerings." +
+        "n- After serving for 10 years, the player will be judged by the people." + 
+        "n- If too many people die during the player’s term of office, the player is removed from" +
+        "\noffice and the game ends.");}
+
+public void whereIsTheCity(){System.out.println(
+        "\nCity of Aaron where on the valley next to City of Amonia City. " +
+        "\nShows where is the city of Aaron" + 
+        "\nFarmland that provide provisions to City of Aaron’s population." +
+        "\nThe fountain that help people and animals to get water and quench thirst." +
+        "\nWithout the fountain all live in the valley they would all be dead." +
+        "\nWe have too pasture that shapers provide provisions to sheep." +
+        "\nAnd in the valley we have a forest that is the border of the Lamanites" +
+        "\nland and we maintain a army to defend us from Lamanites.");}
+
+public void howToviewTheMap(){System.out.println(
+        "\nTo view the map please follow the instruction bellow:" +
+        "\nChose option 1 in the Main Menu, after that you should choose" +
+        "\noptions 1 to view the map.");}
+
+public void howToMoveLocation(){System.out.println(
+        "\nChose option 1 in the Main Menu, after that you should choose " +
+        "\noption 3 to move to another location. Shows how do I move to another location");}
+
+public void howToDisplayStorehouse(){System.out.println(
+        "\"Chose option 1 in the Main Menu, after that you should choose " +
+        "\noption 2 to move to List Menu and in this menu you should choose between those options:\n" +
+        "1 – to view develop team, \n" +
+        "2 – to view a list of animals, \n" +
+        "3 – to view a list of tools, and option \n" +
+        "4 – to view a list of provisions.");}
+
+public void backMainpMenu(){
+           MainMenuView mmv = new MainMenuView();
+           mmv.displayMenu();
+
+}
 }
